c79ed22cda9b6e64070c33664ede29dfb9b88ac5
dashaakvasha <dasha.a.kvasha@gmail.com~>
2026-06-09 14:06:34 +0200

    Add hike types

---
+++ HikeRPG/HikeRPG/Achievements/ElevationAchievement.cs
@@ -0,0 +1,38 @@
+﻿using HikeRPG.Interfaces;
+using HikeRPG.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HikeRPG.Achievements
+{
+    public class ElevationAchievement : IAchievement
+    {
+        private int _minHeight;
+        private bool _unlocked;
+        private string _name;
+
+        public ElevationAchievement(int maxHeight, string name)
+        {
+            _minHeight = maxHeight;
+            _unlocked = false;
+            _name = name;
+        }
+
+        public bool IsUnlocked(CharacterStats stats)
+        {
+            if (stats.TotalElevation >= _minHeight)
+            {
+                _unlocked = true;
+            }
+            return _unlocked;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+}
---
+++ HikeRPG/HikeRPG/Achievements/StreakAchievement.cs
@@ -0,0 +1,39 @@
+﻿using HikeRPG.Interfaces;
+using HikeRPG.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HikeRPG.Achievements
+{
+    public class StreakAchievement:IAchievement
+    {
+        private int _days;
+        private string _name;
+        private bool _unlocked;
+
+
+        public StreakAchievement(int days, string name)
+        {
+            _days = days;
+            _name = name;
+            _unlocked = false;
+        }
+
+        public bool IsUnlocked(CharacterStats stats)
+        {
+            if (stats.CurrentStreak >= _days)
+            {
+                _unlocked = true;
+            }
+            return _unlocked;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+}

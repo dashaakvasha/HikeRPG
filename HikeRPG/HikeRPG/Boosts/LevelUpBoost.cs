68b696b8d9d1f7872dd8660c01694df82607c1a4
dashaakvasha <dasha.a.kvasha@gmail.com~>
2026-06-09 14:46:55 +0200

    Add Boosts

--- HikeRPG/HikeRPG/Boosts/LevelUpBoost.cs
+++ HikeRPG/HikeRPG/Boosts/LevelUpBoost.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HikeRPG.Interfaces;
+using HikeRPG.Models;
 
 namespace HikeRPG.Boosts
 {
-    internal class LevelUpBoost
+    public class LevelUpBoost: IBoost
     {
+        private int _xpBonus;
+        private string _description;
+
+        public LevelUpBoost(int xpBonus, string description)
+        {
+            _xpBonus = xpBonus;
+            _description = description;
+        }
+
+        public void ApplyBoost(CharacterStats stats)
+        {
+            stats.XP += _xpBonus;
+        }
+
+        public string GetDescription()
+        {
+            return _description;
+        }
     }
 }
--- HikeRPG/HikeRPG/Boosts/StreakBoost.cs
+++ HikeRPG/HikeRPG/Boosts/StreakBoost.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using HikeRPG.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HikeRPG.Interfaces;
 
 namespace HikeRPG.Boosts
 {
-    internal class StreakBoost
+    public class StreakBoost: IBoost
     {
+        private int _streakBonus;
+        private string _description;
+
+        public StreakBoost(int streakBonus, string description)
+        {
+            _streakBonus = streakBonus;
+            _description = description;
+        }
+
+        public void ApplyBoost(CharacterStats stats)
+        {
+            stats.Stamina += _streakBonus;
+            stats.Strength += _streakBonus;
+        }
+
+        public string GetDescription() 
+        { 
+            return _description;
+        }
     }
 }

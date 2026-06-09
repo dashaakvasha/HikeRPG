f41ee812db4cb8bd9e3ec00ef392d27349d04dc9
dashaakvasha <dasha.a.kvasha@gmail.com~>
2026-06-09 14:46:55 +0200

    Add Boosts

--- HikeRPG/HikeRPG/Boosts/EnergyBoost.cs
+++ HikeRPG/HikeRPG/Boosts/EnergyBoost.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HikeRPG.Interfaces;
+using HikeRPG.Models;
 
 namespace HikeRPG.Boosts
 {
-    internal class EnergyBoost
+    public class EnergyBoost: IBoost
     {
+        private int _energyAmount;
+        private string _description;
+
+        public EnergyBoost(int energyAmount, string description)
+        {
+            _energyAmount = energyAmount;
+            _description = description;
+        }
+
+        public void ApplyBoost(CharacterStats stats)
+        {
+            stats.Energy += _energyAmount;
+        }
+
+        public string GetDescription()
+        {
+            return _description;
+        }
     }
 }
